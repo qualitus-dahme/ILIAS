7405ca0d2cd2f574168e2a11bd3461afb32ae491
abidzan <abidzan@databay.de>
2025-02-05 13:59:14 +0100

    Restricts users from seeing their learning progress if results are not available before specific date

--- components/ILIAS/Test/classes/class.ilObjTestGUI.php
+++ components/ILIAS/Test/classes/class.ilObjTestGUI.php
@@ -506,8 +506,24 @@ class ilObjTestGUI extends ilObjectGUI implements ilCtrlBaseClassInterface, ilDe
                 if ((!$this->access->checkAccess("read", "", $this->testrequest->getRefId()))) {
                     $this->redirectAfterMissingRead();
                 }
+
+                if ($this->getTestObject() === null) {
+                    $this->infoScreenObject();
+                    break;
+                }
+
+                $test_session = $this->test_session_factory->getSessionByUserId($this->user->getId());
+                if (!$this->getTestObject()->canShowTestResults($test_session)) {
+                    $this->tpl->setOnScreenMessage(
+                        'info',
+                        $this->lng->txt('tst_res_tab_msg_no_lp_access'),
+                    );
+                    break;
+                }
+
                 $this->prepareOutput();
                 $this->addHeaderAction();
+
                 $this->tabs_manager->activateTab(TabsManager::TAB_ID_LEARNING_PROGRESS);
                 $new_gui = new ilLearningProgressGUI(ilLearningProgressGUI::LP_CONTEXT_REPOSITORY, $this->getTestObject()->getRefId());
                 $this->ctrl->forwardCommand($new_gui);

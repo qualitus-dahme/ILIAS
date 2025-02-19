f425c7e419332099f45f614b993e48abb385dc78
Stephan Kergomard <webmaster@kergomard.ch>
2025-02-19 18:18:35 +0100

    Test: Fix Updating Test Results Cache
    See: https://mantis.ilias.de/view.php?id=44082

--- components/ILIAS/Test/classes/class.ilObjTest.php
+++ components/ILIAS/Test/classes/class.ilObjTest.php
@@ -7318,8 +7318,11 @@ class ilObjTest extends ilObject
 
         if ($pass !== null) {
             $query = '
-                SELECT		tst_pass_result.*
+                SELECT		tst_pass_result.*,
+                            tst_active.last_finished_pass
                 FROM		tst_pass_result
+                INNER JOIN  tst_active
+                on          tst_pass_result.active_fi = tst_active.active_id
                 WHERE		active_fi = %s
                 AND			pass = %s
             ';
@@ -7340,7 +7343,7 @@ class ilObjTest extends ilObject
             $percentage = ($max <= 0.0 || $reached <= 0.0) ? 0 : ($reached / $max) * 100.0;
 
             $mark = $this->getMarkSchema()->getMatchingMark($percentage);
-            $is_passed = (bool) $mark->getPassed();
+            $is_passed = $pass <= $test_pass_result_row['last_finished_pass'] && $mark->getPassed();
 
             $hint_count = $test_pass_result_row['hint_count'] ?? 0;
             $hint_points = $test_pass_result_row['hint_points'] ?? 0.0;

42768465a09ef186079857f103798ea5c06fce1f
mjansen <mjansen@databay.de>
2025-02-26 16:50:28 +0100

    Forum: Add checks for commands

--- Modules/Forum/classes/class.ilObjForumGUI.php
+++ Modules/Forum/classes/class.ilObjForumGUI.php
@@ -414,6 +414,8 @@ class ilObjForumGUI extends ilObjectGUI implements ilDesktopItemHandling, ilForu
                 break;
 
             case strtolower(ilObjectContentStyleSettingsGUI::class):
+                $this->checkPermission('write');
+
                 if ($this->in_page_editor_style_context) {
                     $this->tabs_gui->setBackTarget(
                         $this->lng->txt('back'),
@@ -546,6 +548,8 @@ class ilObjForumGUI extends ilObjectGUI implements ilDesktopItemHandling, ilForu
                 break;
 
             case strtolower(ilContainerNewsSettingsGUI::class):
+                $this->checkPermission('write');
+
                 $forum_settings_gui = new ilForumSettingsGUI($this);
                 $forum_settings_gui->settingsTabs();
 
